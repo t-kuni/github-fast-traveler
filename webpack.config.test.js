80488ebe219e77af24de76b6edef289b7c31426d
kunita <kunita@waltsinc.jp>
2019-11-25 14:23:17 +0900

    Modify build speed.

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -15,7 +15,23 @@ module.exports = {
             },
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: [
+                    { loader: 'cache-loader' },
+                    {
+                        loader: 'thread-loader',
+                        options: {
+                            // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                            workers: require('os').cpus().length - 1,
+                            poolTimeout: Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+                        },
+                    },
+                    {
+                        loader : 'ts-loader',
+                        options: {
+                            happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+                        }
+                    }
+                ],
                 exclude: /node_modules/,
             },
         ]
@@ -24,6 +40,6 @@ module.exports = {
         extensions: [ '.tsx', '.ts', '.js', '.vue' ],
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
     ],
 };
